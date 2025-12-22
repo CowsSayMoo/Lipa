72c27601bcb9b77ab4238dc48f6d31e2eacc9c5a
Cowssaymoo <ditiswaarikleer@gmail.com>
2025-12-22 01:45:09 +0100

    testing package installations

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,13 +1,13 @@
 $packageList = @(
-    @{
-        Name = "7zip" 
-        wingetID = "test.test"
-    },
-    @{
-        Name = "Git"
-        wingetID = "Git.git"
-    }
+    @{Name = "7zip"; wingetID = "changme"},
+    @{Name = "Git"; wingetID = "Git.git"}
 )
+
+enum WingetExitCode {
+    Success                  = 0                                    # APPINSTALLER_CLI_ERROR_SUCCESS (or already up-to-date)
+    InstallerFailed          = -1978335200                          # APPINSTALLER_CLI_ERROR_INSTALLER_FAILED (installer-specific error)
+    HashMismatch             = -1978335215                          # APPINSTALLER_CLI_ERROR_INSTALLER_HASH_MISMATCH
+}
 
 $indexList = [System.Collections.Generic.List[int]]::new()
 
@@ -15,11 +15,19 @@ $indexList = [System.Collections.Generic.List[int]]::new()
 foreach ($package in $packageList) {
     $index = $packageList.IndexOf($package)
     winget install --id $package.wingetID --accept-package-agreements --accept-source-agreements --source winget
-    Write-Host($LASTEXITCODE)
     
-    
+    switch ($LASTEXITCODE) {
+        ([WingetExitCode]::Success) {
+             write-host("$($package.Name) installed successfully.") 
+            }
+        [WingetExitCode]::InstallFailed { "$package.Name install failed."; $indexList.Add($index) }
+        [WingetExitCode]::HashMismatch { "$package.Name hash mismatch." }
+        Default {}
+    }
+    $LASTEXITCODE
 }
 
 $indexList
 
 
+# winget uninstall --id Git.Git
