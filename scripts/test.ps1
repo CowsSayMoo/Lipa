e72f392e3e3764f0e2b2e46db954b95ed4746ced
Cowssaymoo <ditiswaarikleer@gmail.com>
2025-12-24 00:05:49 +0100

    night man

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -1,33 +1,45 @@
-$packageList = @(
-    @{Name = "7zip"; wingetID = "changme"},
-    @{Name = "Git"; wingetID = "Git.git"}
+$PackageList = @(
+    @{ Name = "Rustdesk"; WingetId = "RustDesk.RustDesk"; chocoId = "rustdesk" },
+    @{ Name = "Firefox"; WingetId = "Mozilla.Firefox"; chocoId = "firefox" },
+    @{ Name = "Google Chrome"; WingetId = "Google.Chrome"; chocoId = "googlechrome" },
+    @{ Name = "Adobe Acrobat Reader"; WingetId = "Adobe.Acrobat.Reader.64-bit"; chocoId = "adobereader" },
+    @{ Name = "Foxit PDF Reader"; WingetId = "XPFCG5NRKXQPKT"; chocoId = "foxitreader" },
+    @{ Name = "Belgian EID middleware"; WingetId = "BelgianGovernment.eIDmiddleware"; chocoId = "belgian-eid-middleware" },
+    @{ Name = "Belgian EID viewer"; WingetId = "BelgianGovernment.eIDViewer"; chocoId = "belgian-eid-viewer" },
+    @{ Name = "OpenVPN Connect"; WingetId = "OpenVPNTechnologies.OpenVPNConnect"; chocoId = "openvpn-connect" },
+    @{ Name = "VLC Media player"; WingetId = "VideoLAN.VLC"; chocoId = "vlc" },
+    @{ Name = "MS Office 365 Apps"; WingetId = "Microsoft.Office"; chocoId = "office365proplus" }
 )
 
 enum WingetExitCode {
-    Success                  = 0                                    # APPINSTALLER_CLI_ERROR_SUCCESS (or already up-to-date)
-    InstallerFailed          = -1978335200                          # APPINSTALLER_CLI_ERROR_INSTALLER_FAILED (installer-specific error)
-    HashMismatch             = -1978335215                          # APPINSTALLER_CLI_ERROR_INSTALLER_HASH_MISMATCH
+    Success = 0           
+    InstallerFailed = -1978335200
+    HashMismatch = -1978335215   
 }
 
 $indexList = [System.Collections.Generic.List[int]]::new()
 
-
-foreach ($package in $packageList) {
-    $index = $packageList.IndexOf($package)
-    winget install --id $package.wingetID --accept-package-agreements --accept-source-agreements --source winget
+foreach ($package in $PackageList) {
+    $index = $PackageList.IndexOf($package)
+    $installed = winget list --id $package.WingetId --exact | Out-String | Should -Not -Match "$($package.Name) - $($package.Version)"
     
-    switch ($LASTEXITCODE) {
-        ([WingetExitCode]::Success) {
-             write-host("$($package.Name) installed successfully.") 
+    if (-not $installed) {
+        winget install --id $($package.WingetId) --accept-package-agreements --accept-source-agreements --source winget
+        switch ($LASTEXITCODE) {
+            ([WingetExitCode]::Success) { 
+                write-host("$($package.Name) installed successfully.") 
             }
-        [WingetExitCode]::InstallFailed { "$package.Name install failed."; $indexList.Add($index) }
-        [WingetExitCode]::HashMismatch { "$package.Name hash mismatch." }
-        Default {}
+            ([WingetExitCode]::InstallerFailed) { 
+                write-host("$($package.Name) install failed."); $indexList.Add($index) 
+            }
+            ([WingetExitCode]::HashMismatch) { 
+                write-host("$($package.Name) hash mismatch."); $indexList.Add($index) 
+            }
+            Default { 
+                write-host("$($package.Name) install failed."); $indexList.Add($index) 
+            }
+        }
     }
-    $LASTEXITCODE
 }
 
 $indexList
-
-
-# winget uninstall --id Git.Git
